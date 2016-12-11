60b16d191fbd10dc8adce100c2e09942f8f9810b
fedxr <you2xr@126.com>
2016-12-11 15:43:52 +0800

    add commits to biffer.js

--- buffer.js
+++ buffer.js
@@ -1,4 +1,6 @@
+//二进制缓冲区
+
 var utf8String = 'my string';
 var buf = new Buffer(utf8String);
-var base64String = buf.toString('base64');
+var base64String = buf.toString('base64'); //解码
 console.log(base64String);
