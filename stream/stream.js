eb3f0b01aa78052abf1c6c380c61ab2a9171f1e9
fedxr <you2xr@126.com>
2016-12-20 23:14:01 +0800

    a node demo about http read and write stream

--- stream/stream.js
+++ stream/stream.js
@@ -1,19 +1,34 @@
 //避免慢客户端问题
-require('http').createServer(function (req, res) {
+var fs = require('fs');
+// require('http').createServer(function (req, res) {
 
+// 	var rs = fs.createReadStream('../buffer.js');
+
+// 	rs.on('data', function(data) {
+// 		if (!res.write(data)) {
+// 			rs.pause();
+// 		}
+// 	});
+
+// 	res.on('drain', function() {
+// 		rs.resume();
+// 	});
+
+// 	rs.on('end', function() {
+// 		res.end();
+// 	});
+// }).listen(8080);
+
+
+
+//用stream.pipe()避免慢客户端问题
+require('http').createServer(function (req, res) {
 	var rs = fs.createReadStream('../buffer.js');
 
-	rs.on('data', function(data) {
-		if (!res.write(data)) {
-			rs.pause();
-		}
-	});
-
-	res.on('drain', function() {
-		rs.resume();
-	});
+	rs.pipe(res, {end: false});
 
 	rs.on('end', function() {
+		res.write('And that is all, folks!');
 		res.end();
 	});
 }).listen(8080);
