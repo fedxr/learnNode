1e886ed1dcee86ffcb37a93a6760dff4bafbd85e
fedxr <you2xr@126.com>
2017-01-16 00:09:29 +0800

    comprehend serverRespose

--- HTTP/req_headers.js
+++ HTTP/req_headers.js
@@ -2,6 +2,11 @@
 
 var util = require('util');
 require('http').createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end(util.inspect(req.headers));  //util.inspect(),是一个可以分析任意对象属性的实用函数
+	res.writeHead(200, {
+		'Content-Type': 'text/plain',
+		'Cache-Control': 'max-age=3600'
+	});
+	// res.end('hello world');
+	// res.end(req.url);  //url包含路径。如：http://127.0.0.1:4000/abc中的abc
+	res.end(util.inspect(req.headers)); //util.inspect(),是一个可以分析任意对象属性的实用函数
 }).listen(4000);
